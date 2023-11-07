e57a929f9869c6a6b4a93f0741f00308471ba2be
Yuichi Hirano <chirano@uraroji.com>
2023-11-08 07:07:54 +0900

    Update LICENSE

--- LicensePlistViewController/LicensePlistParser.swift
+++ LicensePlistViewController/LicensePlistParser.swift
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2018-2022 Yuichi Hirano
+// Copyright (c) 2018-2023 Yuichi Hirano
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
